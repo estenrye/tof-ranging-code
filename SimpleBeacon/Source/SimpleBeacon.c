11417461135db0d40a7213cea5f050cad2b808cb
Esten Rye <esten@ryezone.net>
2018-04-30 16:45:44 -0500

    Add minimum code to compile for Simple Beacon

---
+++ SimpleBeacon/Source/SimpleBeacon.c
@@ -0,0 +1,110 @@
+/****************************************************************************/
+/***        Include files                                                 ***/
+/****************************************************************************/
+
+#include <jendefs.h>
+#include <AppHardwareApi.h>
+#include <string.h>
+#include "LcdDriver.h"
+#include "AlsDriver.h"
+#include "HtsDriver.h"
+#include "Button.h"
+#include "LedControl.h"
+#include "Jenie.h"
+#include "Utils.h"
+
+/****************************************************************************
+ *
+ * NAME: vJenie_ConfigureNetwork
+ *
+ * DESCRIPTION:
+ * Entry point for application from boot loader. Initialises system.
+ *
+ * RETURNS:
+ * Nothing
+ *
+ ****************************************************************************/
+PUBLIC void vJenie_CbConfigureNetwork(void)
+{
+	/* to be implemented */
+}
+
+PUBLIC void vJenie_CbInit(bool_t bWarmStart)
+{
+	/* to be implemented */
+}
+
+/****************************************************************************
+ *
+ * NAME: vJenie_HwEvent
+ *
+ * DESCRIPTION:
+ * Adds events to the hardware event queue.
+ *
+ * PARAMETERS:      Name            RW  Usage
+ *                  u32Device       R   Peripheral responsible for interrupt e.g DIO
+ *                  u32ItemBitmap   R   Source of interrupt e.g. DIO bit map
+ *
+ * RETURNS:
+ * void
+ *
+ ****************************************************************************/
+PUBLIC void vJenie_CbHwEvent(uint32 u32DeviceId,uint32 u32ItemBitmap)
+{
+	/* to be implemented */
+}
+
+/****************************************************************************
+ *
+ * NAME: vJenie_Main
+ *
+ * DESCRIPTION:
+ * Main user routine. This is called by the Basic Operating System (BOS)
+ * at regular intervals.
+ *
+ * RETURNS:
+ * void
+ *
+ ****************************************************************************/
+PUBLIC void vJenie_CbMain(void)
+{
+	/* to be implemented */
+}
+
+/****************************************************************************
+ *
+ * NAME: vJenie_StackMgmtEvent
+ *
+ * DESCRIPTION:
+ * Used to receive stack management events
+ *
+ * PARAMETERS:      Name                    RW  Usage
+ *                  *psStackMgmtEvent       R   Pointer to event structure
+ *
+ * RETURNS:
+ * void
+ *
+ ****************************************************************************/
+PUBLIC void vJenie_CbStackMgmtEvent(teEventType eEventType, void *pvEventPrim)
+{
+	/* to be implemented */
+}
+
+/****************************************************************************
+ *
+ * NAME: vJenie_StackDataEvent
+ *
+ * DESCRIPTION:
+ * Used to receive stack data events
+ *
+ * PARAMETERS:      Name                    RW  Usage
+ *                  *psStackDataEvent       R   Pointer to data structure
+ *
+ * RETURNS:
+ * void
+ *
+ ****************************************************************************/
+PUBLIC void vJenie_CbStackDataEvent(teEventType eEventType, void *pvEventPrim)
+{
+	/* to be implemented */
+}
